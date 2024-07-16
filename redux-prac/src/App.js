cb667d59389916d2685453b2035a78656d09a1dc
colorkite10 <colorkite10@gmail.com>
2024-07-16 12:47:14 +0900

    init: 리덕스 설치 및 store.js 만듦

---
+++ redux-prac/src/App.js
@@ -0,0 +1,13 @@
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-prac/src/redux/store.js
@@ -0,0 +1,9 @@
+// 리덕스는 스토어에 모든 state 상태값을 저장한다.
+// 리덕스 안 쓰면 부모 컴포넌트에 있는 변수를 자식 컴포넌트가 못 건듦
+// 그래서 아예 스토어라는 다른 곳에 변수를 모두 모아서 원격으로 들고 옴
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+  reducer: {}, //상태관리 할 것들을 저장할 공간
+});
