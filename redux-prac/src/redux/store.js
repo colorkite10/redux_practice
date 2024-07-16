a9bcf22f825aac4275a1772d3e7e4ec9d13f94f3
colorkite10 <colorkite10@gmail.com>
2024-07-16 13:04:18 +0900

    feat: reducer 만들고 store에 저장

--- redux-prac/src/redux/store.js
+++ redux-prac/src/redux/store.js
@@ -3,7 +3,10 @@
 // 그래서 아예 스토어라는 다른 곳에 변수를 모두 모아서 원격으로 들고 옴
 
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user";
 
 export default configureStore({
-  reducer: {}, //상태관리 할 것들을 저장할 공간
+  reducer: {
+    user: userReducer, //userSlice.reducer로 export했는데 어떻게 이걸로 불러오지?
+  }, //상태관리 할 것들을 저장할 공간
 });
---
+++ redux-prac/src/redux/user.js
@@ -0,0 +1,22 @@
+// 스토어가 식당이라면 리듀서는 접시다.
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  //createSlice는 기존에 createReducer와 createAction이 하던 일을 같이 해줌.
+  //쉽게 말해 actions를 위한 js파일을 따로 만들 필요가 없음!
+  name: "user", //리듀서 이름을 뭘로 할지 정하기
+  initialState: { value: { name: "", age: 0, email: "" } }, //들어갈 데이터의 초기값
+  reducers: {
+    //상태가 변하면 어떻게 실행될지 정하는 부분
+    //로그인 버튼을 눌렀을 때 이름, 나이, 이메일이 변하게 하고 싶으니
+    //로그인 함수를 만들어준다.
+    login: (state, action) => {
+      //state: 우리가 잡아 놓은 초기값의 value를 가져오는 역할
+      //action: 바꾸고 싶은 데이터를 원하는 곳에 넘겨주는 역할
+      state.value = action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
