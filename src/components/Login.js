fbf207e5b2776f6959fbb2d694079f42e28f8108
colorkite10 <colorkite10@gmail.com>
2024-07-16 13:49:43 +0900

    feat: 로그아웃 버튼 누르면 초기값으로 돌아감

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { login } from "../redux/user";
+import { login, logout } from "../redux/user";
 
 function Login() {
   const dispatch = useDispatch();
   //이걸 쓰면 action을 보낼 수 있다.
 
-  const onClick = () => {
+  const handleLoginButtonClick = () => {
     dispatch(login({ name: "내 이름", age: 20, email: "email@gmail.com" }));
+  };
+
+  const handleLogoutButtonClick = () => {
+    dispatch(logout());
   };
 
   return (
     <div>
-      <button onClick={onClick}>Login</button>
+      <button onClick={handleLoginButtonClick}>Login</button>
+      <button onClick={handleLogoutButtonClick}>Logout</button>
     </div>
   );
 }
--- src/redux/user.js
+++ src/redux/user.js
@@ -2,11 +2,13 @@
 
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialStateValue = { name: "", age: 0, email: "" };
+
 export const userSlice = createSlice({
   //createSlice는 기존에 createReducer와 createAction이 하던 일을 같이 해줌.
   //쉽게 말해 actions를 위한 js파일을 따로 만들 필요가 없음!
   name: "user", //리듀서 이름을 뭘로 할지 정하기
-  initialState: { value: { name: "", age: 0, email: "" } }, //들어갈 데이터의 초기값
+  initialState: { value: initialStateValue }, //들어갈 데이터의 초기값
   reducers: {
     //상태가 변하면 어떻게 실행될지 정하는 부분
     //로그인 버튼을 눌렀을 때 이름, 나이, 이메일이 변하게 하고 싶으니
@@ -16,10 +18,13 @@ export const userSlice = createSlice({
       //action: 바꾸고 싶은 데이터를 원하는 곳에 넘겨주는 역할
       state.value = action.payload;
     },
+    logout: (state) => {
+      state.value = initialStateValue;
+    },
   },
 });
 
-export const { login } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 //login 함수를 action 기능이 작동하도록 다른 데서 쓸 것
 
 export default userSlice.reducer;
